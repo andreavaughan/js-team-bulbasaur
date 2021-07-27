171fe0a6d7fe70a45f4abba82a21b16b9b96d80a
andreavaughan <amvaughan16@gmail.com>
2021-07-27 13:37:35 -0400

    in progress search fetch request

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 const form = document.querySelector('#artist-form')
-const url = 'http://127.0.0.1:8080'
+const url = 'https://proxy-itunes-api.glitch.me/search?term='
 const movieList = document.querySelector('#movie-list')
+const artistText = document.getElementById('artist-text').value
+const searchToString = String(artistText)
+const searchParameter = url + searchToString
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-    const artistText = document.getElementById('artist-text').value
     console.log(artistText)
-   
+    
+    console.log(searchParameter)
+    returnSearch()
     // this will clear my input after submitting a todo
     form.reset()
   })
 
-  // const url = 'https://itunes.apple.com/search?'
   // // const form = need to get form id from html
   // // const sontList = need to get list id from html
   
   // //Event listeners
   
-  // form.addEventListener('submit', function (event) {
-  //     event.preventDefault()
-  //     const //create a variable that contains the value of the search
-  // })
+
+function returnSearch() {
+  fetch(searchParameter)
+    .then((response) => response.json())
+    .then((data) => {
+      for (const song of data) {
+        console.log(song)
+      }
+    })
+}
 
  
 
