bf9d4ac89f398c15d6cf1a555b62b295ab63f8d4
andreavaughan <amvaughan16@gmail.com>
2021-07-27 12:03:54 -0400

    started working on javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+const url = 'https://itunes.apple.com/search?'
+// const form = need to get form id from html
+// const sontList = need to get list id from html
+
+//Event listeners
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const //create a variable that contains the value of the search
+})
