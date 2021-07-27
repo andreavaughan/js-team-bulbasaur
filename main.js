1a92771c1167a0e67072c1117f0bc2112295ae83
andreavaughan <amvaughan16@gmail.com>
2021-07-27 16:25:20 -0400

    in-progress search parameter testing

--- main.js
+++ main.js
@@ -2,31 +2,31 @@ const form = document.querySelector('#artist-form')
 const url = 'https://proxy-itunes-api.glitch.me/search?term='
 const movieList = document.querySelector('#movie-list')
 const artistText = document.getElementById('artist-text').value
-const searchToString = String(artistText)
-const searchParameter = url + searchToString
+
+  //Event listener
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
     console.log(artistText)
-    
-    console.log(searchParameter)
-    returnSearch()
-    // this will clear my input after submitting a todo
+    searchParameter()
+    // returnSearch()
     form.reset()
   })
 
-  // // const form = need to get form id from html
-  // // const sontList = need to get list id from html
-  
-  // //Event listeners
-  
+function searchParameter(){
+  const searchToString = String(artistText)
+  console.log(searchToString)
+  const refactoredSearch = encodeURIComponent(searchToString).replace(/%20/g, "+")
+  console.log(refactoredSearch)
+}
 
 function returnSearch() {
-  fetch(searchParameter)
+  fetch(searchParameter())
     .then((response) => response.json())
     .then((data) => {
       for (const song of data) {
         console.log(song)
+
       }
     })
 }
