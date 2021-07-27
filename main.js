ffa3cbb1650d50344f2055ceaacb9edc5464b9b0
andreavaughan <amvaughan16@gmail.com>
2021-07-27 16:46:12 -0400

    working on addSearchParameter fn

--- main.js
+++ main.js
@@ -1,35 +1,41 @@
 const form = document.querySelector('#artist-form')
-const url = 'https://proxy-itunes-api.glitch.me/search?term='
-const movieList = document.querySelector('#movie-list')
-const artistText = document.getElementById('artist-text').value
+const url = 'https://proxy-itunes-api.glitch.me/search?'
+// const movieList = document.querySelector('#movie-list')
+
+
 
   //Event listener
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-    console.log(artistText)
-    searchParameter()
+    addSearchParameter ()
+
     // returnSearch()
     form.reset()
   })
 
-function searchParameter(){
+function addSearchParameter (){
+  const artistText = document.getElementById('artist-text').value
   const searchToString = String(artistText)
+  console.log(artistText)
   console.log(searchToString)
+
   const refactoredSearch = encodeURIComponent(searchToString).replace(/%20/g, "+")
   console.log(refactoredSearch)
 }
 
-function returnSearch() {
-  fetch(searchParameter())
-    .then((response) => response.json())
-    .then((data) => {
-      for (const song of data) {
-        console.log(song)
 
-      }
-    })
-}
+
+// function returnSearch() {
+//   fetch(searchParameter())
+//     .then((response) => response.json())
+//     .then((data) => {
+//       for (const song of data) {
+//         console.log(song)
+
+//       }
+//     })
+// }
 
  
 
