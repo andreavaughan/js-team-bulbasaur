c1768d7e785df33ecd521cb86de2db6085ef6eb3
andreavaughan <amvaughan16@gmail.com>
2021-07-28 14:00:41 -0400

    in progress render search fn

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('search-form')
 const url = 'https://proxy-itunes-api.glitch.me/search?term=ariana+grande&country=us&entity=song&limit=5'
+const searchList = document.getElementById('search-list')
 
 
 
@@ -15,6 +16,20 @@ form.addEventListener('submit', function (event) {
     returnSearch()
     form.reset()
 })
+
+//This function renders the search results on the page
+function renderSearchResults(songObj) {
+  let resultItem = document.createElement('li')
+  resultItem.classList.add('card')
+  searchResultsText(resultItem, songObj)
+  searchList.appendChild(resultItem)
+}
+
+//This function adds the text of the search results on the page
+function searchResultsText (songItem, songObj){
+  songItem.innerHTML = `<span>${songObj}</span>`
+}
+
 
 //This function is in progress to add characters to search value so that they match the parameters
 function addSearchParameter (){
@@ -38,7 +53,7 @@ function returnSearch() {
       let results = data.results
         for (let song of results){
           console.log(song)
-          //add function to display results on page
+          renderSearchResults(song)
       }
   })
 }
